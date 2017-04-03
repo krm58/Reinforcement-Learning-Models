035c2ccc325e2cecfae7085269d8e4c7786a2adf
Kelsey McDonald <krm58@duke.edu>
2017-04-03 16:54:32 -0400

    Add files via upload

---
+++ BanditTask_AsymmetricRL/DiffPEModelFitDriver.m
@@ -0,0 +1,58 @@
+%RUNS FMINCON
+%this script is run once
+
+load MTurkHoneyData; % the data to fit
+Nsubjects = size(dataTest.stimChosen,1); 
+global V_end;
+V_end = zeros(Nsubjects,6);
+
+clear Fit 
+Fit.Nparms = 3;
+Fit.LB = [0 0 1e-6];
+Fit.UB = [1 1 30];
+
+for s = 1:Nsubjects;
+    fprintf('Fitting subject %d out of %d...\n',s,Nsubjects)
+    % preprocessing the data a bit
+    T = dataTraining.trialtype(s,:);
+    T = T+1;
+    C = dataTraining.stimChosen(s,:) - 64;
+    Acc = dataTraining.accuracy(s,:);
+    R = dataTraining.outcome(s,:);
+    
+    for iter = 1:10;   % run 10 times from random initial conditions, to get best fit
+        fprintf('Iteration %d...\n',iter)
+        
+        % determining initial condition
+        Fit.init(s,iter,:) = rand(1,length(Fit.LB)).*(Fit.UB-Fit.LB)+Fit.LB; % random initialization
+        
+        % running fmincon to fit the free parameters of the model
+        [res,lik] = ... 
+            fmincon(@(x) FitModel_DiffPE(C,R,x(1),x(2),x(3),s),...
+            squeeze(Fit.init(s,iter,:)),[],[],[],[],Fit.LB,Fit.UB,[],...
+            optimset('TolX', 0.00001, 'TolFun', 0.00001, 'MaxFunEvals', 9e+9, 'Algorithm', 'interior-point'));
+        % GradObj = 'on' to use gradients, 'off' to not use them *** ask us about this if you are interested *** 
+        % DerivativeCheck = 'on' to have fminsearch compute derivatives numerically and check the ones I supply
+        % LargeScale = 'on' to use large scale methods, 'off' to use medium
+        Fit.Result.EtaPos(s,iter) = res(1); 
+        Fit.Result.EtaNeg(s,iter) = res(2);
+        Fit.Result.Beta(s,iter) = res(3); 
+        Fit.Result.Lik(s,iter) = lik;
+        Fit.Result.Lik  % to view progress so far
+    end
+    
+end
+
+% find the best fit results for each subject
+%Displays subject id, eta, beta, loglikelihood
+[a,b] = min(Fit.Result.Lik,[],2);
+for s = 1:Nsubjects
+    Fit.Result.BestFit(s,:) = [s,...
+    Fit.Result.EtaPos(s,b(s)),... 
+    Fit.Result.EtaNeg(s,b(s)),...
+    Fit.Result.Beta(s,b(s)),...
+    Fit.Result.Lik(s,b(s))];
+end
+Fit.Result.BestFit
+Fit = Fit.Result.BestFit
+save('DiffPEModelFits','Fit','V_end')
---
+++ BanditTask_AsymmetricRL/FitModel_DiffOutcome.m
@@ -0,0 +1,65 @@
+function [lik] = FitModel_DiffOutcome(CSchosen,Rewards,etaPos,etaNeg,beta,s)
+%LOG LIKELIHOOD FUNCTION,  run many times
+%eta = learning rate
+
+% Find the log likelihood of the choice data under a TD model with learning rate
+% eta, softmax temperature beta
+
+% Outputs:
+% Lik = - log likelihood of the data
+
+lik    = 0;   % log likelihood
+
+%in each pair, even is good, odd is bad
+CS_num = [1 2; 3 4; 5 6]; % The stimuli used for each trial type
+
+V = [0, 0, 0, 0, 0, 0];  % initial values for the 6 stimuli
+global V_end;
+for t = 1:length(CSchosen)  % t = trial number
+    c = 0;
+    n = 0;
+    %pick the current belief on the rewards to come from each action
+    if (isnan(CSchosen(t)))
+        continue
+    end
+    if (CSchosen(t) == 1) %if they chose A, they didn't choose B
+        c = 1;
+        n = 2;
+    elseif (CSchosen(t) == 2)
+        c = 2;
+        n = 1;
+    elseif (CSchosen(t) == 3)
+        c = 3;
+        n=4;
+    elseif (CSchosen(t) == 4)
+        c = 4;
+        n=3;
+    elseif (CSchosen(t) == 5)
+        c = 5;
+        n = 6;
+    elseif (CSchosen(t) == 6)
+        c = 6;
+        n=5;
+    end
+    
+    qt = [V(c), V(n)];
+    
+    % adding this choice to the log likelihood
+    x = beta*V(c) - log(sum(exp(beta*qt)));
+    lik = lik + x;
+    
+    % updating the value of the chosen stimulus according to the reward received (on choice as well as single trials)
+    PE = Rewards(t) - V(c);
+    
+    if (Rewards(t) > 0)
+        V(c) = V(c) + PE*etaPos;
+    else V(c) = V(c) + PE*etaNeg;
+    end
+end
+
+% OPTIONAL: putting a prior on the parameters (so we are looking for the MAP and not the ML solution)
+lik = lik + log(betapdf(etaPos,1.1,1.1));
+lik = lik + log(betapdf(etaNeg,1.1,1.1));
+lik = lik + log(gampdf(beta,2,3));
+lik = -lik;  % so we can minimize the function rather than maximize
+V_end(s,:) = V;
